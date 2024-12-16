ad3eb838ffb39c30f3e276f3e5d6e5bd13aeb38c
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-12-16 23:04:32 +0100

    added book1/ch02/hello{1,2}.c

---
+++ book1/ch02/hello1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* main: generate some simple output */
+
+int main(void) {
+  printf("Hello World.\n");
+  printf("How are you?\n");
+  return (EXIT_SUCCESS);
+}
---
+++ book1/ch02/hello2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* main: generate some simple output */
+
+int main(void) {
+  printf("Goodbye, ");
+  printf("cruel world!\n");
+  printf("Just kidding. There's no time to say goodbye!\n");
+  return (EXIT_SUCCESS);
+}
