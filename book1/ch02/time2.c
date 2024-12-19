0676121fd2afc1cf82b7868e83607b88c4499a8f
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-12-19 22:31:02 +0100

    added book1/ch02/time{1,2}.c

---
+++ book1/ch02/time2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  int hour = 11;
+  int minute = 59;
+  char colon = ':';
+
+  printf("The hardcoded time is %i%c%i\n", hour, colon, minute);
+
+  return (EXIT_SUCCESS);
+}
