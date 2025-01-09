d02dda0bfee668caa50cace5c89889b8e4a78a0b
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-01-09 20:25:20 +0100

    cosmetic changes

--- book1/ch07/main_text/randArray.c
+++ book1/ch07/main_text/randArray.c
@@ -11,6 +11,16 @@ int getRandInt(int lowBound, int upBound) {
   return getRand(upBound - lowBound) + lowBound;
 }
 
+/* funny, in C you cannot determine the size of an array in the function */
+/* or at least it is not trivial */
+void printArray(int array[], int length) {
+  printf("[");
+  for (int i = 0; i < length; i++) {
+    printf(" %i ", array[i]);
+  }
+  printf("]\n");
+}
+
 int main(void) {
 
   srand(time(NULL));
@@ -21,14 +31,9 @@ int main(void) {
     arr[i] = getRandInt(1, 11);
   }
 
-  printf("Printing %i-element array with random ints: 1 upto 10 inside\n",
+  printf("Printing %i-element array with random ints 1 upto 10 inside\n",
          arrLen);
-
-  printf("[");
-  for (int i = 0; i < arrLen; i++) {
-    printf(" %i ", arr[i]);
-  }
-  printf("]\n");
+  printArray(arr, arrLen);
 
   return (EXIT_SUCCESS);
 }
