de9f4870f3e523459529b07179f6edf4fbd63a54
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-01-09 20:33:03 +0100

    added book1/ch07/main_text/counting.c

---
+++ book1/ch07/main_text/counting.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* returns a random int in range 0 (inclusive) and upBound (exclusive) */
+int getRand(int upBound) { return rand() % upBound; }
+
+/* returns a random int in range lowBound (inclusive) and upBound (exclusive)
+ */
+int getRandInt(int lowBound, int upBound) {
+  return getRand(upBound - lowBound) + lowBound;
+}
+
+/* funny, in C you cannot determine the size of an array in the function */
+/* or at least it is not trivial */
+void printArray(int array[], int length) {
+  printf("[");
+  for (int i = 0; i < length; i++) {
+    printf(" %i ", array[i]);
+  }
+  printf("]\n");
+}
+
+int countVals(int array[], int length, int value) {
+  int count = 0;
+  for (int i = 0; i < length; i++) {
+    if (array[i] == value) {
+      count++;
+    }
+  }
+  return count;
+}
+
+int main(void) {
+
+  srand(time(NULL));
+  int arrLen = getRandInt(5, 10);
+  int arr[arrLen];
+  for (int i = 0; i < arrLen; i++) {
+    srand(time(NULL) + i);
+    arr[i] = getRandInt(1, 11);
+  }
+
+  printf("Counting 1s in a random array with random ints 1 upto 10 inside\n");
+  printArray(arr, arrLen);
+  printf("Result: %i\n", countVals(arr, arrLen, 1));
+
+  return (EXIT_SUCCESS);
+}
