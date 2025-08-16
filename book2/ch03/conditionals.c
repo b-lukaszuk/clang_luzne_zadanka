102f00d7be86ecc4900d2085f9615a1515b52879
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-08-16 09:35:37 +0200

    added book2/c03/conditionals.c

---
+++ book2/ch03/conditionals.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  int x = 21;
+  if (x > 10 && x < 100) {
+    puts("x is greater than 10 and less than 100!");
+
+  } else {
+    puts("x is less than 11 or greater than 99!");
+  }
+  return 0;
+}
