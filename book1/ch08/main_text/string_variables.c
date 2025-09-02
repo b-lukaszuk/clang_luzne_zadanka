a2fcaab39b27cc532d67ac05c4a0fc3318ce7933
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-09-02 20:21:00 +0200

    added book1/ch08/main_text/string_variables.c

---
+++ book1/ch08/main_text/string_variables.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+  char first[] = "Hello, ";
+  char second[] = "world!";
+  printf("%s%s composed of strings, i.e. arrays of chars.\n", first, second);
+  return 0;
+}
