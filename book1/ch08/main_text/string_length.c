a847dc7004742b937641433ca48b7a5c4b84ea20
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-09-09 20:26:00 +0200

    cosmetic changes in book1/ch08/main_text/string_length.c

--- book1/ch08/main_text/string_length.c
+++ book1/ch08/main_text/string_length.c
@@ -3,10 +3,13 @@
 #define EXIT_SUCCESS 0;
 
 int main(void) {
-    int length;
+    int length = 0;
     char fruit[] = "banana";
+    char lastLetter = ' ';
 
     length = strlen(fruit);
+    lastLetter = fruit[length-1];
     printf("The length of the word '%s' is %d characters.\n", fruit, length);
+    printf("The last letter of the word '%s' is '%c'.\n", fruit, lastLetter);
     return EXIT_SUCCESS;
 }
