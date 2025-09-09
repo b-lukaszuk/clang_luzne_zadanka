13f90f171eeb879e2ec3518d8f3da83c606d5129
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-09-09 20:38:58 +0200

    added book1/ch08/main_text/str_traversal.c

---
+++ book1/ch08/main_text/str_traversal.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#define EXIT_SUCCESS 0;
+
+int main(void) {
+    char fruit[] = "banana";
+    int length = strlen(fruit);
+    int index = length - 1;
+
+    printf("The the word '%s' spelled backwards is: '", fruit);
+    while (0 <= index) {
+        printf("%c", fruit[index]);
+        index = index - 1;
+    }
+    printf("'.\n");
+    return EXIT_SUCCESS;
+}
