56f9570637415827ecae38e68f01bb594a99663f
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-09-10 19:20:16 +0200

    added book1/ch08/main_text/str_concat.c

---
+++ book1/ch08/main_text/str_concat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#define EXIT_SUCCESS 0;
+
+int main(void) {
+  char fruit[20] = "banana";
+  char bakedGood[] = " nut bread";
+
+  printf("concatenating '%s' and '%s'\n", fruit, bakedGood);
+  strncat(fruit, bakedGood, 10);
+  printf("result: '%s'\n", fruit);
+
+  return EXIT_SUCCESS;
+}
