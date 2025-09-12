c379035480a74f55ca9850d066f1739f9926321c
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-09-12 21:16:22 +0200

    added book1/ch08/main_text/str_characterization.c

---
+++ book1/ch08/main_text/str_characterization.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include <stdio.h>
+#define EXIT_SUCCESS 0;
+
+int main(void) {
+  char letter = 'a';
+  if (isalpha(letter)) {
+    printf("The character '%c' is a letter.\n", letter);
+    printf("The uppercased '%c' is '%c'.\n", letter, toupper(letter));
+  }
+
+  return EXIT_SUCCESS;
+}
