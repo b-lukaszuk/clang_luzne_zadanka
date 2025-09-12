db69397a9f07d6692b3e997eab64b0f6a2afd43f
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-09-12 21:22:52 +0200

    added book1/ch08/main_text/get_usr_input.c

---
+++ book1/ch08/main_text/get_usr_input.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define EXIT_SUCCESS 0;
+
+int main(void) {
+  int success, x;
+  // prompt the user for input
+  printf("Enter an integer: \n");
+
+  // get input
+  success = scanf("%i", &x);
+
+  // check and see if the input statement succeeded
+  if (success == 1) {
+    // print the value we got from the user
+    printf("Your input: %i\n", x);
+    return EXIT_SUCCESS;
+  }
+  printf("That was not an integer.\n");
+  return EXIT_SUCCESS;
+}
