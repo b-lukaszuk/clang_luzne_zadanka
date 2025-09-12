5ad2c22455eeb4713276a0cf9fff91af2f157637
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-09-12 21:09:52 +0200

    cosmetic changes

--- book1/ch08/main_text/str_assign.c
+++ book1/ch08/main_text/str_assign.c
@@ -5,7 +5,9 @@
 int main(void) {
   char greeting[15];
   printf("Current value of 'greeting' variable is: '%s'\n", greeting);
-  // 14th caracter is the invisible string end character in C: '\0'
+  // 14th caracter is the invisible string end character in C: '\0' (backslash zero)
+  // it is not counted by strlen() from string.h, so strlen("Hello, world!") is 13
+  // still, you need to consider it in strncpy(), so that it is copied (automatically) as well
   strncpy(greeting, "Hello, world!", 14);
   // while coping:
   // char greeting[6];
