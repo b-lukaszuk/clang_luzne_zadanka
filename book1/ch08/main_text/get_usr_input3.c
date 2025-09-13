5027d6e4ab2f2decdc035090c14ebf5b0047e637
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-09-13 20:14:19 +0200

    added book1/ch08/main_text/get_usr_input3.c

---
+++ book1/ch08/main_text/get_usr_input3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define EXIT_SUCCESS 0;
+
+int main(void) {
+  char name[80];
+  printf("What is your name?\n");
+  scanf("%s", name);
+  printf("Hello %s, nice to meet you.\n", name);
+  return EXIT_SUCCESS;
+}
