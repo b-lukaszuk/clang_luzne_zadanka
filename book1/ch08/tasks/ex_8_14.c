daa764daa89c7c473b70dfaabac19a556a128a37
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-09-14 23:54:36 +0200

    added isAbecedarianRec to book1/ch08/tasks/ex_8_14.c + cosmetic

--- book1/ch08/tasks/ex_8_14.c
+++ book1/ch08/tasks/ex_8_14.c
@@ -14,11 +14,34 @@ int isAbecedarianIter(char *s) {
   return TRUE;
 }
 
+int isAbecedarianRec(char *s, int i) {
+  if (i < 1 || i >= strlen(s)) {
+    i = 1;
+  }
+  if (i >= strlen(s) || i + 1 >= strlen(s)) {
+    return TRUE;
+  } else if (s[i - 1] >= s[i]) {
+    return FALSE;
+  } else {
+    return isAbecedarianRec(s, i + 1);
+  }
+}
+
 int main(void) {
   char word1[] = "biopsy";
-  char word2[] = "bussy";
-  char word3[] = "";
-  printf("Is '%s' abecedarian (0 - false, 1 - true)? %d\n", word1, isAbecedarianIter(word1));
-  printf("Is '%s' abecedarian (0 - false, 1 - true)? %d\n", word2, isAbecedarianIter(word2));
+  char word2[] = "busy";
+
+  printf("Testing function isAbecedarianIter()\n");
+  printf("Is '%s' abecedarian (0 - false, 1 - true)? %d\n", word1,
+         isAbecedarianIter(word1));
+  printf("Is '%s' abecedarian (0 - false, 1 - true)? %d\n", word2,
+         isAbecedarianIter(word2));
+
+  printf("\nTesting function isAbecedarianRec()\n");
+  printf("Is '%s' abecedarian (0 - false, 1 - true)? %d\n", word1,
+         isAbecedarianRec(word1, 1));
+  printf("Is '%s' abecedarian (0 - false, 1 - true)? %d\n", word2,
+         isAbecedarianRec(word2, 1));
+
   return EXIT_SUCCESS;
 }
